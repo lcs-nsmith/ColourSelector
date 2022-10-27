ab70c97ab1a02860ea7f3f73a8534c6685662199
Joyce Tao <jtao@lcs.on.ca>
2022-10-27 11:15:20 -0400

    Initial Commit

---
+++ ColourSelector/ColourSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColourSelectorApp.swift
+//  ColourSelector
+//
+//  Created by Joyce Tao on 2022-10-27.
+//
+
+import SwiftUI
+
+@main
+struct ColourSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
